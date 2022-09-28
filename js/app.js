beadec9f48bb5d245087f6f0bbaa3c4dfdbeb5cc
Abdalrhman8000 <abdo.eslam2021@gmail.com>
2022-09-28 15:57:07 +0200

    Firts Push

---
+++ js/app.js
@@ -0,0 +1,23 @@
+class Shadow {
+    constructor() {
+        this.set = (data) => {
+            if (data) {
+                Appender(data);
+            } else {
+                console.warn("Please Enter Array Of Data");
+            }
+        };
+
+        let Appender = (items) => {
+            document.addEventListener("DOMContentLoaded", () => {
+                const data = items;
+                for (let item of data) {
+                    let elements = document.querySelectorAll(`.${item.name}`);
+                    elements.forEach((e) => {
+                        e.classList.add(...data[data.indexOf(item)].sty);
+                    });
+                }
+            });
+        };
+    }
+}
